4998684a2b0d7e36d09430d57d3370c26bbb96ae
Abelj <111173212+abeljemal22@users.noreply.github.com>
2023-01-21 14:07:14 +0300

    sort.h

--- sort.h
+++ sort.h
@@ -6,26 +6,17 @@
 #include <time.h>
 
 /**
- *struct listint_s - Doubly linked list node
- *@n: Integer stored in the node
-
+ * struct listint_s - Doubly linked list node
  *
- *     * @prev: Pointer to the previous element of the list
- *
- *      * @next: Pointer to the next element of the list
- *
- *       */
-
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
 typedef struct listint_s
-
 {
-
-		int n;
-
-			struct listint_s *prev;
-
-				struct listint_s *next;
-
+	int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 
@@ -35,6 +26,7 @@ void print_list(const listint_t *list);
 void check_list(const listint_t *list);
 void check_array(const int *array, size_t size);
 int *rand_array(int len, int max);
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
@@ -48,6 +40,4 @@ void heap_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 
-
-
-#endifi
+#endif
