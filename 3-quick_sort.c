37c7c54fda99a0329a934f51e3f18910e72c6585
Abelj <111173212+abeljemal22@users.noreply.github.com>
2023-01-21 17:34:45 +0300

    sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,65 +1,76 @@
 #include "sort.h"
 
 /**
+ * swap - swaps 2 int values
+ * @array: the integer array to sort
+ * @size: the size of the array
+ * @a: address of first value
+ * @b: address of second value
  *
- *  *selection_sort- function that sorts using selection sort algorithm
- *
- *   *
- *
- *    *@array: array to be sorted
- *
- *     *@size: size of the array
- *
- *      *
- *
- *       */
-
-void selection_sort(int *array, size_t size)
-
+ * Return: void
+ */
+void swap(int *array, size_t size, int *a, int *b)
 {
+	if (*a != *b)
+	{
+		*a = *a + *b;
+		*b = *a - *b;
+		*a = *a - *b;
+		print_array((const int *)array, size);
+	}
+}
 
-		size_t i, j, k;
+/**
+ * lomuto_partition - partitions the array
+ * @array: the integer array to sort
+ * @size: the size of the array
+ * @lo: the low index of the sort range
+ * @hi: the high index of the sort range
+ *
+ * Return: void
+ */
+size_t lomuto_partition(int *array, size_t size, ssize_t lo, ssize_t hi)
+{
+	int i, j, pivot = array[hi];
 
-			int temp;
+	for (i = j = lo; j < hi; j++)
+		if (array[j] < pivot)
+			swap(array, size, &array[j], &array[i++]);
+	swap(array, size, &array[i], &array[hi]);
 
+	return (i);
+}
 
+/**
+ * quicksort - quicksorts via Lomuto partitioning scheme
+ * @array: the integer array to sort
+ * @size: the size of the array
+ * @lo: the low index of the sort range
+ * @hi: the high index of the sort range
+ *
+ * Return: void
+ */
+void quicksort(int *array, size_t size, ssize_t lo, ssize_t hi)
+{
+	if (lo < hi)
+	{
+		size_t p = lomuto_partition(array, size, lo, hi);
 
-				if (!array || !size)
+		quicksort(array, size, lo, p - 1);
+		quicksort(array, size, p + 1, hi);
+	}
+}
 
-							return;
-
-					for (i = 0; i < size - 1; i++)
-
-							{
-
-										for (j = size - 1, k = i + 1; j > i; j--)
-
-													{
-
-																	if (array[j] < array[k])
-
-																					{
-
-																										k = j;
-
-																													}
-
-																			}
-
-												if (array[i] > array[k])
-
-															{
-
-																			temp = array[i];
-
-																						array[i] = array[k];
-
-																									array[k] = temp;
-
-																												print_array(array, size);
-
-																														}
-
-													}
-
+/**
+ * quick_sort - calls quicksort
+ * @array: the integer array to sort
+ * @size: the size of the array
+ *
+ * Return: void
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (!array || !size)
+		return;
+	quicksort(array, size, 0, size - 1);
 }
